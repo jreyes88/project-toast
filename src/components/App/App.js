3cce4695c0539cb8882bce46613ca7e5caf9cac0
jjooeeyy <jreyes88@gmail.com>
2024-10-14 09:21:58 -0500

    Exercise 4 done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-
-import ToastPlayground from '../ToastPlayground';
-import Footer from '../Footer';
+import React from "react";
+import ToastProvider from "../ToastProvider";
+import ToastPlayground from "../ToastPlayground";
+import Footer from "../Footer";
 
 function App() {
   return (
     <>
-      <ToastPlayground />
+      <ToastProvider>
+        <ToastPlayground />
+      </ToastProvider>
       <Footer />
     </>
   );
--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -6,7 +6,7 @@ import {
   Info,
   X,
 } from "react-feather";
-
+import { ToastContext } from "../ToastProvider";
 import VisuallyHidden from "../VisuallyHidden";
 
 import styles from "./Toast.module.css";
@@ -18,7 +18,8 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast({ id, variant, handleDismiss, children }) {
+function Toast({ id, variant, children }) {
+  const { dismissToast } = React.useContext(ToastContext);
   const Icon = ICONS_BY_VARIANT[variant];
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
@@ -29,7 +30,7 @@ function Toast({ id, variant, handleDismiss, children }) {
       <button
         className={styles.closeButton}
         onClick={() => {
-          handleDismiss(id);
+          dismissToast(id);
         }}
       >
         <X size={24} />
